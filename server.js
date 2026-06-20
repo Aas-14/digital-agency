12509ce518f9a483f9ff9590e724c7fe522d341c
Aas-14 <borkhadeaastha@gmail.com>
2026-06-20 19:40:33 +0530

    Fix Render PORT issue

--- server.js
+++ server.js
@@ -50,10 +50,6 @@ app.post("/contact", async (req, res) => {
 
   const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT} 🚀`);
-});
-
 //   /* ❌ COMMENTED DATABASE INSERT
 //   const sql =
 //     "INSERT INTO contacts (name,email,message) VALUES (?,?,?)";
