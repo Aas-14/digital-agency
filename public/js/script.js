05c7981ae104c2797551fadacdefffc6ba13246d
Aas-14 <borkhadeaastha@gmail.com>
2026-06-20 15:06:00 +0530

    First website upload

---
+++ public/js/script.js
@@ -0,0 +1,195 @@
+// HERO BUTTON
+
+document.getElementById('btn')?.addEventListener('click', () => {
+    document.querySelector('#contact')
+    .scrollIntoView({
+        behavior:'smooth'
+    });
+});
+
+
+// CONTACT FORM
+
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    try {
+
+        const res = await fetch(
+            "http://127.0.0.1:5000/contact",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type":"application/json"
+                },
+                body: JSON.stringify({
+                    name,
+                    email,
+                    message
+                })
+            }
+        );
+
+        // no response.json()
+
+        if(res.ok){
+            alert("Message Saved Successfully ✅");
+            form.reset();
+        } else {
+            alert("Failed to send ❌");
+        }
+
+    } catch(err){
+        console.log(err);
+        alert("Backend not connected ❌");
+    }
+});
+
+// REVEAL ANIMATION
+
+const cards =
+document.querySelectorAll(
+    '.card'
+);
+
+const observer =
+new IntersectionObserver(
+
+(entries)=>{
+
+entries.forEach(
+(entry)=>{
+
+if(entry.isIntersecting){
+
+entry.target.style.opacity =
+'1';
+
+entry.target.style.transform =
+'translateY(0)';
+
+}
+
+});
+
+},
+
+{
+threshold:.2
+}
+
+);
+
+cards.forEach((card)=>{
+
+card.style.opacity='0';
+
+card.style.transform=
+'translateY(50px)';
+
+card.style.transition=
+'.7s';
+
+observer.observe(card);
+
+});
+
+// MOBILE MENU
+
+const menuToggle =
+document.querySelector('.menu-toggle');
+
+const navLinks =
+document.querySelector('.nav-links');
+
+menuToggle.addEventListener(
+'click',
+()=>{
+    navLinks.classList.toggle('active');
+}
+);
+
+// REVEAL ANIMATION
+
+const reveals =
+document.querySelectorAll('.reveal');
+
+window.addEventListener(
+'scroll',
+()=>{
+
+reveals.forEach(section=>{
+
+const top =
+section.getBoundingClientRect().top;
+
+if(top < window.innerHeight-100){
+section.classList.add('active');
+}
+
+});
+
+});
+
+// PREMIUM COUNTER
+/*
+const counters =
+document.querySelectorAll(
+'.stat-box h2'
+);
+
+const speed = 120;
+
+counters.forEach(
+(counter)=>{
+
+const target =
+counter.innerText;
+
+const number =
+parseInt(
+target.replace(/\D/g,'')
+);
+
+let count = 0;
+
+const updateCounter = ()=>{
+
+const increment =
+Math.ceil(number/speed);
+
+if(count < number){
+
+count += increment;
+
+if(count > number){
+count = number;
+}
+
+if(target.includes('%')){
+counter.innerText =
+count + '%';
+}
+else{
+counter.innerText =
+count + '+';
+}
+
+requestAnimationFrame(
+updateCounter
+);
+
+}
+};
+
+updateCounter();
+
+});*/
+
+
---
+++ server.js
@@ -0,0 +1,104 @@
+
+require('dotenv').config();
+console.log("EMAIL_USER =", process.env.EMAIL_USER);
+console.log("EMAIL_PASS =", process.env.EMAIL_PASS ? "Loaded ✅" : "Missing ❌");
+//Email notification
+const nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
+
+transporter.verify(function(error, success) {
+    if (error) {
+        console.log("Mail Error:", error);
+    } else {
+        console.log("Mail Server Ready ✅");
+    }
+});
+
+
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+db.connect((err)=>{
+    if(err){
+        console.log('MySQL Error:', err);
+    }else{
+        console.log('MySQL Connected ✅');
+    }
+});
+app.post("/contact", (req, res) => {
+
+    const { name, email, message } = req.body;
+
+    const sql =
+    "INSERT INTO contacts (name,email,message) VALUES (?,?,?)";
+
+    db.query(sql, [name, email, message], async (err, result) => {
+
+            if (err) {
+                console.log(err);
+                return res.status(500).json({
+                    success:false,
+                    message:"Database Error"
+                });
+            }
+
+            // EMAIL CODE GOES HERE 👇
+try {
+
+    const info = await transporter.sendMail({
+        from: process.env.EMAIL_USER,
+        to: process.env.EMAIL_USER,
+        subject: "New Website Lead - Just Media 05",
+        html: `
+            <h2>New Contact Form Submission</h2>
+            <p><strong>Name:</strong> ${name}</p>
+            <p><strong>Email:</strong> ${email}</p>
+            <p><strong>Message:</strong> ${message}</p>
+        `
+    });
+
+    console.log("Email Sent:", info.response);
+
+} catch(error) {
+
+    console.log("Email Error:", error);
+
+}
+            return res.status(200).json({
+                success:true,
+                message:"Message Saved Successfully ✅"
+            });
+        }
+    );
+});
+
+const PORT =
+process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log(
+        `Server running on ${PORT} 🚀`
+    );
+});
